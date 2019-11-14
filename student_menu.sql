8571d920ead94be33bbeca73cbad90ddd970d45d
Zhen Huang <zhuang38@hawk.iit.edu>
2019-11-14 14:24:38 -0600

    procedure for student menu

---
+++ student_menu.sql
@@ -0,0 +1,13 @@
+delimiter $$
+drop procedure if exists student_menu $$
+create procedure student_menu(
+	in sid char(20),
+	in y char(20),
+	in sem char(20)
+)
+begin
+select UoSCode from transcript where 
+Studid = sid and Year = y and Semester = sem;
+end $$
+delimiter ;
+
