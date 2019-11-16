7aa8ad28cc8b325ddb9984998eba3b84d6aac673
Zhen Huang <zhuang38@hawk.iit.edu>
2019-11-15 23:16:45 -0600

    update for flag_tag table that contain info for warning

--- enrollment_check.sql
+++ enrollment_check.sql
@@ -1,7 +1,7 @@
 delimiter $$
 drop trigger if exists enrollment_check $$
 create trigger enrollment_check
-after update on uosoffering
+after delete on uosoffering
 for each row
 begin
     if (new.MaxEnrollment > 2*new.Enrollment and new.MaxEnrollment <= 2* (new.Enrollment + 1))
