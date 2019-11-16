96108da5dc1c5c843731fbcd6ec9e7e5f2a1a632
mhjasonliu <jasonliu2020.1@u.northwestern.edu>
2019-11-15 20:27:27 -0600

    authentication of student code

---
+++ AuthStudent.sql
@@ -0,0 +1,12 @@
+DROP PROCEDURE IF EXISTS AuthStudent;
+
+delimiter //
+CREATE PROCEDURE AuthStudent (IN username VARCHAR(20), IN passw VARCHAR(20))
+BEGIN
+	SELECT * FROM student
+    WHERE Name = username AND Password = passw;
+END //
+delimiter ;
+
+CALL AuthStudent("Harry Jenkins","butterflY");
+
