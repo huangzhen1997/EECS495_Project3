b9b0e244a3d4377d8eb22d82b610c32625a4c44b
Zhen Huang <zhuang38@hawk.iit.edu>
2019-11-15 18:13:22 -0600

    Update enrollment_check.sql

--- enrollment_check.sql
+++ enrollment_check.sql
@@ -4,7 +4,7 @@ create trigger enrollment_check
 after update on uosoffering
 for each row
 begin
-    if (new.MaxEnrollment > 2*new.Enrollment)
+    if (new.MaxEnrollment > 2*new.Enrollment and new.MaxEnrollment <= 2* (new.Enrollment + 1))
     then SIGNAL sqlstate '45000' set message_text = 'The Enrollment number now is below 50 !!';
     end if;
 end $$
