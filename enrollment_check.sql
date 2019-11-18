2f0387f9cab608182343987564ac289f381d43a7
Zhen Huang <zhuang38@hawk.iit.edu>
2019-11-17 22:15:49 -0600

    Revert "enrollment check, check table flag_tag after each withdraw"
    This reverts commit a66e9ffc9a3f1c807168e7992f731336f1b46015.

--- enrollment_check.sql
+++ enrollment_check.sql
@@ -1,18 +1,12 @@
-drop table if exists flag_tag;
-create table flag_tag(tag varchar(5) primary key, val integer, UoSCode char(8), Semester char(2), Year int(11));
-insert into flag_tag values('flag',0,'','',0);
 delimiter $$
 drop trigger if exists enrollment_check $$
 create trigger enrollment_check
 after update on uosoffering
 for each row
 begin
-    if (new.MaxEnrollment > 2*new.Enrollment and new.MaxEnrollment<= 2* (new.Enrollment+1))
-    then update flag_tag set val = 1,UoSCode = new.UoSCode,Semester = new.Semester,Year = new.Year where tag = 'flag';
-    elseif (new.MaxEnrollment <= 2*new.Enrollment)
-    then update flag_tag set val = 0 where tag = 'flag';
+    if (new.MaxEnrollment > 2*new.Enrollment and new.MaxEnrollment <= 2* (new.Enrollment + 1))
+    then 
+    update tmp set flag = 1;
     end if;
 end $$
 delimiter ;
-
-select * from flag_tag;
