3eeb2b3751c2a33bce62ba76fb4b1bb0159f5583
Zhen Huang <zhuang38@hawk.iit.edu>
2019-11-17 22:26:00 -0600

    fix transcript

--- Transcript.sql
+++ Transcript.sql
@@ -4,6 +4,8 @@ create procedure transcript(
 	in sid char(20)
 )
 begin
-select UoSCode,Year,Grade,Semester from student natural join transcript where id = sid order by Year;
+select UoSCode,Year,Grade,Semester from student as s natural join transcript as t where s.id = t.StudId and s.id = sid order by Year;
 end $$
 delimiter ;
+
+call transcript(5123)
